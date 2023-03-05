3c5f642dad06adb7054137c186cf7cc5f32ce5a6
Jamila <jamnjeri@gmail.com>
2023-03-06 01:03:52 +0300

    Added a Delete button and its functionality

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,7 +5,7 @@ import styles from "./dashboard.css"
 function Dashboard({ userInfo }) {
 
     const [memes, setMemes]=useState([]);
-    console.log(userInfo)
+    // console.log(userInfo)
 
     useEffect(() => {
         fetch('http://localhost:9292/memes')
@@ -13,14 +13,47 @@ function Dashboard({ userInfo }) {
         .then(data => setMemes(data));
     },[]);
 
+
+    function addMeme(){
+
+    }
+
+    function updateMeme(){
+        
+    }
+
+    function deleteMeme(meme_id, user_id){
+        console.log(meme_id)
+        console.log(user_id)
+        console.log(userInfo.data)
+        if(user_id === userInfo.data.id){
+            console.log("Possible")
+            // DELETE
+            fetch(`http://localhost:9292/memes/${meme_id}`, {
+                method: "DELETE"
+            })
+            .then(response => response.json())
+            .then(() => {
+                // Remove the deleted name from the state
+                setMemes(memes.filter(meme => meme.id !== meme_id))
+            })
+            .catch(error => console.log(error));
+        }
+        else{
+            console.log("Not Possible")
+            alert('Sorry, the meme you are trying to delete was created by another user');
+        }
+    }
+
   return (
     <>
-    <h1>Welcome </h1>
+    <h1>Welcome To The Meme Library: </h1>
     <div className="card-container">
         {memes.map((meme, index) => (
-        <Memes key={index} meme={meme} />
+        <Memes key={index} meme={meme} deleteMeme={deleteMeme} />
         ))}
     </div>
+    <h1>Add your own Memes:</h1>
     </>
 
   );
--- src/components/Memes.js
+++ src/components/Memes.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import styles from "./dashboard.css"
 
-function Memes({ meme }) {
+function Memes({ meme, deleteMeme }) {
+
+    function handleDelete(){
+        deleteMeme(meme.id, meme.user_id)
+    }
+
   return (
     <div className='card'>
         <img className='card-img' src={meme.url} alt={meme.name} />
         <div className="card-body">
             <h2 className="card-title">{meme.name}</h2>
-            <button>Delete</button>
+            <h4 className='card-name'>Added By: {meme.user.full_name}</h4>
+            <button onClick={handleDelete}>Delete</button>
+            <button>Update</button>
         </div>
     </div>
   )
